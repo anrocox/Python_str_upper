cd51517b40f3cf2f5dc42793f93ec9bc1e7f903b
Andres Rodriguez <anrocox@gmail.com>
2014-07-08 14:18:20 -0500

    How to convert characters to uppercase in a python str?

---
+++ src/upper_str.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 8, 2014
+
+@author: anroco
+
+How to convert characters to uppercase in a python str?
+
+¿Como convertir los caracteres de un str python en mayusculas?
+'''
+
+#create a str
+s = 'Example String in Python'
+print(s)
+
+#the upper() method generates copy of the string with all the cased characters
+#converted to uppercase.
+s_upper = s.upper()
+print(s_upper)
+print(s)
+
+#create a str
+s = 'create 2 games in 1 minute'
+print(s)
+
+#return a new string.
+s_upper = s.upper()
+print(s_upper)
